d598d6c7fe9772c23331e4763a9281730bdd8a25
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-25 14:36:57 -0400

    fix

--- chperms.c
+++ chperms.c
@@ -9,17 +9,17 @@
 #include <liboldworld.h>
 
 int main(const int argc, const char *argv[]) {
-    if (geteuid() != 0) { printlog(LOGTYPE_ERROR, "Must be run as root or setuid"); } else
+    if (geteuid() != 0) { printlog(LOGTYPE_ERROR, "Must be run as root or setuid"); exit(EXIT_FAILURE); }
 
-    if (argc < 2) { fprintf(stderr, "Usage: chperms <file1> [file2] [file3]\n"); exit(EXIT_FAILURE); } else
+    if (argc < 2) { fprintf(stderr, "Usage: chperms <file1> [file2] [file3]\n"); exit(EXIT_FAILURE); }
 
     for (int i = 1; i < argc; i++) {
         const char *file = realpath(argv[i], NULL);
-        if (file == NULL) { printlog(LOGTYPE_ERROR, "%s not found", argv[i]); } else
-        if (strncmp(file, "/srv/", 5) != 0) { printlog(LOGTYPE_ERROR, "%s is not in /srv", argv[i]); } else {
+        if (file == NULL) { printlog(LOGTYPE_ERROR, "%s not found", argv[i]); exit(EXIT_FAILURE); }
+        if (strncmp(file, "/srv/", 5) != 0) { printlog(LOGTYPE_ERROR, "%s is not in /srv", argv[i]); exit(EXIT_FAILURE); }
 
         const char *user = strtok(strdup(file) + 5, "/");
-        if (getpwnam(user) == NULL) { printlog(LOGTYPE_ERROR, "User %s does not exist", user); } }
+        if (getpwnam(user) == NULL) { printlog(LOGTYPE_ERROR, "User %s does not exist", user); exit(EXIT_FAILURE); }
     }
 
     for (int i = 1; i < argc; i++) {
@@ -30,7 +30,7 @@ int main(const int argc, const char *argv[]) {
 
         int perms = 0664;
         if (getfiletype(file) == 1) { perms = 02775; }
-        if (chmod(file, perms) != 0) { printlog(LOGTYPE_ERROR, "Failed to change permissions for %s", argv[i]); } else
-        if (chown(file, uid, gid) != 0) { printlog(LOGTYPE_ERROR, "Failed to change ownership for %s", argv[i]); }
+        if (chmod(file, perms) != 0) { printlog(LOGTYPE_ERROR, "Failed to change permissions for %s", argv[i]); exit(EXIT_FAILURE); }
+        if (chown(file, uid, gid) != 0) { printlog(LOGTYPE_ERROR, "Failed to change ownership for %s", argv[i]); exit(EXIT_FAILURE); }
     }
 }
