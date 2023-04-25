d4c627a91dea7de613bf776c1e0bf4a0fb35d446
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-25 14:33:16 -0400

    best

--- chperms.c
+++ chperms.c
@@ -8,31 +8,29 @@
 #include <unistd.h>
 #include <liboldworld.h>
 
-int main(int argc, char *argv[]) {
-    if (geteuid() != 0) { printerr("Must be run as root or setuid"); }
+int main(const int argc, const char *argv[]) {
+    if (geteuid() != 0) { printlog(LOGTYPE_ERROR, "Must be run as root or setuid"); } else
 
-    if (argc < 2) { fprintf(stderr, "Usage: chperms <file1> [file2] [file3]\n"); exit(EXIT_FAILURE); }
+    if (argc < 2) { fprintf(stderr, "Usage: chperms <file1> [file2] [file3]\n"); exit(EXIT_FAILURE); } else
 
     for (int i = 1; i < argc; i++) {
-        char *file = realpath(argv[i], NULL);
-        if (file == NULL) { printerr("%s not found", argv[i]); }
-        if (strncmp(file, "/srv/", 5) != 0) { printerr("%s is not in /srv", argv[i]); }
+        const char *file = realpath(argv[i], NULL);
+        if (file == NULL) { printlog(LOGTYPE_ERROR, "%s not found", argv[i]); } else
+        if (strncmp(file, "/srv/", 5) != 0) { printlog(LOGTYPE_ERROR, "%s is not in /srv", argv[i]); } else {
 
-        char *user = strtok(strdup(file) + 5, "/");
-        struct passwd *pw = getpwnam(user);
-        if (pw == NULL) { printerr("User %s does not exist", user); }
+        const char *user = strtok(strdup(file) + 5, "/");
+        if (getpwnam(user) == NULL) { printlog(LOGTYPE_ERROR, "User %s does not exist", user); } }
     }
 
     for (int i = 1; i < argc; i++) {
-        char *file = realpath(argv[i], NULL);
-        char *user = strtok(strdup(file) + 5, "/");
-        struct passwd *pw = getpwnam(user);
-        uid_t uid = pw->pw_uid;
-        gid_t gid = pw->pw_gid;
+        const char *file = realpath(argv[i], NULL);
+        const struct passwd *pw = getpwnam(strtok(strdup(file) + 5, "/"));
+        const uid_t uid = pw->pw_uid;
+        const gid_t gid = pw->pw_gid;
 
         int perms = 0664;
         if (getfiletype(file) == 1) { perms = 02775; }
-        if (chmod(file, perms) != 0) { printerr("Failed to change permissions for %s", argv[i]); }
-        if (chown(file, uid, gid) != 0) { printerr("Failed to change ownership for %s", argv[i]); }
+        if (chmod(file, perms) != 0) { printlog(LOGTYPE_ERROR, "Failed to change permissions for %s", argv[i]); } else
+        if (chown(file, uid, gid) != 0) { printlog(LOGTYPE_ERROR, "Failed to change ownership for %s", argv[i]); }
     }
 }
